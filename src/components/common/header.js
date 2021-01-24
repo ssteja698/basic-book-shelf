dcc8d20f00e235cc3add60de95dc1fe12fb1bf62
ssteja698 <>
2021-01-25 00:24:31 +0530

    basic version

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { createBrowserHistory } from 'history';
 import './header.css';
 
+import { isMobileDevice } from '../../helpers/Utils.js';
+
+const IS_MOBILE = isMobileDevice();
+
 const history = createBrowserHistory({
   forceRefresh: true,
 });
@@ -19,64 +23,76 @@ class Header extends Component {
     const headings = ['BookID', 'Title', 'Authors', 'Average Rating', 'ISBN', 'Language Code', 'Ratings', 'Price'];
 
     return (
-    <div style={{ margin: '40px 0px' }}>
-      <div onClick={() => this.setState({ showCart: false })} style={{ cursor: 'pointer', padding: 5, borderRadius: '50%', position: 'absolute', left: 70, top: 40, fontSize: 20 }}>
+    <div style={{ margin: IS_MOBILE ? '140px 0px' : '100px 0px' }}>
+      <div onClick={() => this.setState({ showCart: false })} style={{ color: '#23394C', cursor: 'pointer', padding: 5, borderRadius: '50%', position: 'absolute', left: IS_MOBILE ? 30 : 70, top: 100, fontSize: 20 }}>
         {"<- Back"}
       </div>
-      <div style={{ fontSize: 24, textDecoration: 'underline' }}>
-        You added the following items into the Cart:
-      </div>
-      <div style={{ margin: 'auto', width: '90%', overflowX: 'auto' }}>
-        <table
-            className='table table-hover'
-            style={{
-                width: '100%', margin: '10px auto', boxShadow: '1px 2px 6px #8B9DAF33', borderRadius: 4, backgroundColor: '#FFFFFF', border: '1px solid #CDCFD6',
-            }}
-        >
-            <thead style={{ display: 'flex', background: '#F6F8FA', color: '#23394C' }}>
-                <tr
-                    style={{
-                        width: '100%', fontSize: '18px', fontWeight: '700', color: '#11426C',
-                    }}
-                >
-                    {headings.map((heading, columnIndex) => (
-                        <th
-                            style={{
-                                minWidth: columnIndex === 1 ? '58%' : '9%', textAlign: 'left', border: '1px solid #DFE3EA', paddingRight: 10, paddingLeft: 10,
+      {(selectedBooks.length === 0) && (
+        <div style={{ color: '#23394C', fontSize: IS_MOBILE ? 18 : 24, textDecoration: 'underline' }}>
+          Your Cart is Empty
+        </div>
+      )}
+      {(selectedBooks.length > 0) && (
+        <>
+          <div style={{ color: '#23394C', fontSize: IS_MOBILE ? 18 : 24, textDecoration: 'underline' }}>
+            You added the following items into the Cart:
+          </div>
+          <div style={{ width: '90%', display: 'flex', flexDirection: 'column', margin: '10px auto', overflowX: 'auto' }}>
+            <table
+                className="table table-hover"
+                style={{
+                    width: '100%', boxShadow: '1px 2px 6px #8B9DAF33', borderRadius: 4, backgroundColor: '#FFFFFF', border: '1px solid #CDCFD6',
+                }}
+            >
+                <thead>
+                    <tr
+                        className="table-header"
+                        style={{
+                            fontSize: '18px', fontWeight: '700', height: 55, color: '#6465A5',
+                        }}
+                    >
+                        {headings.map((heading, index) => {
+                            let width = '10%';
+                            if (index === 0) {
+                            width = '5%';
+                            } else if (index === 2) {
+                            width = '30%';
+                            }
+                            return (
+                            <th style={{
+                                width, borderRight: (index !== headings.length - 1) && '1px solid #DFE3EA', textAlign: 'center', borderBottom: '1px solid #DFE3EA',
                             }}
-                        >
-                          {heading}
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {selectedBooks.map((book, index) => {
+                            >
+                                {heading.toLocaleUpperCase()}
+                            </th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {selectedBooks.map((book) => {
                     const rowData = Object.values(book);
                     return (
-                        <tr
-                            style={{
-                              width: '100%', display: 'flex', background: '#F6F8FA', color: '#23394C'
-                            }}
-                        >
-                            {rowData.map((data, columnIndex) => {
-                                return (
-                                    <td
-                                        key={`${index} - ${columnIndex}`}
-                                        style={{
-                                            overflow: 'auto', width: columnIndex === 1 ? '60%' : '8%', textAlign: 'left', paddingRight: 10, paddingLeft: 10, border: '1px solid #DFE3EA',
-                                        }}
-                                    >
-                                        {data}
-                                    </td>
-                                )
-                            })}
+                        <tr style={{ fontSize: 16, fontWeight: '400', borderBottom: '1px solid #DFE3EA' }}>
+                            {rowData.map((key, columnIndex) => {
+                              return (
+                                  <td
+                                      role="presentation"
+                                      style={{
+                                          padding: '12px', borderRight: (columnIndex !== rowData.length - 1) && '1px solid #DFE3EA', borderBottom: '1px solid #DFE3EA',
+                                      }}
+                                      >
+                                      {key}
+                                  </td>
+                              );})}
                         </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-      </div>
+                    );
+                    })}
+                </tbody>
+            </table>
+          </div>
+        </>
+      )}
     </div>
     )
   }
@@ -93,8 +109,8 @@ class Header extends Component {
       >
         <div style={{ fontWeight: 800 }}>
           <span style={{ fontSize: 30 }}>
-            <span style={{ color: '#282726' }}>BOOK</span>
-            <span style={{ color: '#6A8A82' }}>POINT</span>
+            <span style={{ color: '#F1E0D6' }}>BOOK</span>
+            <span style={{ color: '#BF988F' }}>PLAZA</span>
           </span>
         </div>
       </div>
@@ -102,29 +118,30 @@ class Header extends Component {
   }
 
   render() {
-    const { children, selectedBooks } = this.props;
-
-    if(this.state.showCart) {
-      return this.renderCart();
-    }
+    const { children } = this.props;
 
     return (
       <>
-        <div className="Header" style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div className="Header" style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: '#6465A5' }}>
           <div className="headerLeft" style={{ display: 'flex', alignItems: 'center', paddingLeft: '5%' }}>
             {this.renderHeaderLogo()}
           </div>
-          <div 
-            className="Button" 
-            style={{ color: '#FFF', padding: '5px 2%', marginRight: '5%', backgroundColor: '#6465A5' }}
-            onClick={() => this.setState({ showCart: true })}
-          >
-            Add to Cart
+          {!this.state.showCart && (
+            <div 
+              className="Button" 
+              style={{ color: '#FFF', padding: '5px 2%', marginRight: '5%', backgroundColor: '#BF988F' }}
+              onClick={() => this.setState({ showCart: true })}
+            >
+              Cart
+            </div>
+          )}
+        </div>
+        {this.state.showCart && this.renderCart()}
+        {!this.state.showCart && (
+          <div style={{ marginTop: 70 }}>
+            {children}
           </div>
-        </div>
-        <div style={{ marginTop: 70 }}>
-          {children}
-        </div>
+        )}
       </>
     );
   }
--- src/components/home.js
+++ src/components/home.js
@@ -16,18 +16,18 @@ class Home extends PureComponent {
             books: [],
             filteredBooks: [],
             searchedValue: '',
-            pageCount: 10,
+            pageCount: 7,
             startIndex: 0,
             currentPage: 1,
             isSearched: false,
             cart: '',
             selectedBookIds: [],
         };
-        this.handleSearch = _.debounce(this.handleSearch.bind(this), 500);
+        this.handleSearch = _.debounce(this.handleSearch.bind(this), 200);
     }
 
     componentDidMount() {
-        axios.get(`https://s3-ap-southeast-1.amazonaws.com/he-public-data/books8f8fe52.json`)
+        axios.get('https://raw.githubusercontent.com/ssteja698/basic-book-shelf/main/book-data.json')
         .then(res => {
             const books = res.data;
             this.setState({ books });
@@ -73,7 +73,7 @@ class Home extends PureComponent {
 
     renderBooks() {
         const { currentPage, pageCount, books, filteredBooks, searchedValue, isSearched } = this.state;
-        const headings = ['Mark', 'BookID', 'Title', 'Authors', 'Average Rating', 'ISBN', 'Language Code', 'Ratings', 'Price'];
+        const headings = ['Add to Cart', 'BookID', 'Title', 'Authors', 'Average Rating', 'ISBN', 'Language Code', 'Ratings', 'Price'];
         const booksList = isSearched ? filteredBooks.slice((currentPage - 1) * pageCount, currentPage * pageCount) : books.slice((currentPage - 1) * pageCount, currentPage * pageCount);
 
         return (
@@ -87,70 +87,77 @@ class Home extends PureComponent {
                     }}
                     placeholder="Search"
                 />
-                <div style={{ margin: 'auto', width: '90%', overflowX: 'auto' }}>
+                <div style={{ width: '90%', display: 'flex', flexDirection: 'column', margin: '10px auto', overflowX: 'auto' }}>
                     <table
-                        className='table table-hover'
+                        className="table table-hover"
                         style={{
-                            width: '100%', margin: '10px auto', boxShadow: '1px 2px 6px #8B9DAF33', borderRadius: 4, backgroundColor: '#FFFFFF', border: '1px solid #CDCFD6',
+                            width: '100%', boxShadow: '1px 2px 6px #8B9DAF33', borderRadius: 4, backgroundColor: '#FFFFFF', border: '1px solid #CDCFD6',
                         }}
                     >
-                        <thead style={{ width: '100%', display: 'flex', background: '#F6F8FA', color: '#23394C' }}>
+                        <thead>
                             <tr
+                                className="table-header"
                                 style={{
-                                    width: '100%', fontSize: '18px', fontWeight: '700', color: '#11426C',
+                                    fontSize: '18px', fontWeight: '700', height: 55, color: '#6465A5',
                                 }}
                             >
-                                {headings.map((heading, columnIndex) => (
-                                    <th
-                                        style={{
-                                            overflow: 'auto', minWidth: columnIndex === 2 ? '58%' : '9%', textAlign: 'left', border: '1px solid #DFE3EA', paddingRight: 10, paddingLeft: 10,
-                                        }}
+                                {headings.map((heading, index) => {
+                                    let width = '10%';
+
+                                    if (index === 0) {
+                                    width = '5%';
+                                    } else if (index === 2) {
+                                    width = '30%';
+                                    }
+                                    return (
+                                    <th style={{
+                                        width, borderRight: (index !== headings.length - 1) && '1px solid #DFE3EA', textAlign: 'center', borderBottom: '1px solid #DFE3EA',
+                                    }}
                                     >
-                                        {heading}
+                                        {heading.toLocaleUpperCase()}
                                     </th>
-                                ))}
+                                    );
+                                })}
                             </tr>
                         </thead>
                         <tbody>
-                            {booksList.map((book, index) => {
-                                const rowData = Object.values(book);
-                                return (
-                                    <tr
-                                        style={{
-                                            width: '100%', display: 'flex', background: '#F6F8FA', color: '#23394C'
-                                        }}
-                                    >
-                                        <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>
-                                            <input
-                                                type="checkbox"
-                                                checked={this.state.selectedBookIds.indexOf(book.bookID) > -1}
-                                                onChange={() => this.handleSelectedBooks(book.bookID)}
-                                            />
-                                        </td>
-                                        {rowData.map((data, columnIndex) => {
-                                            return (
-                                                <td
-                                                    key={`${index} - ${columnIndex}`}
-                                                    style={{
-                                                        overflow: 'auto', width: columnIndex === 1 ? '60%' : '8%', textAlign: 'left', paddingRight: 10, paddingLeft: 10, border: '1px solid #DFE3EA',
-                                                    }}
+                            {booksList.map((book) => {
+                            const rowData = Object.values(book);
+                            return (
+                                <tr style={{ fontSize: 16, fontWeight: '400', borderBottom: '1px solid #DFE3EA' }}>
+                                    <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>
+                                        <input
+                                            type="checkbox"
+                                            style={{ cursor: 'pointer' }}
+                                            checked={this.state.selectedBookIds.indexOf(book.bookID) > -1}
+                                            onChange={() => this.handleSelectedBooks(book.bookID)}
+                                        />
+                                    </td>
+                                    {
+                                        rowData.map((key, columnIndex) => {
+                                        return (
+                                            <td
+                                                role="presentation"
+                                                style={{
+                                                    padding: '12px', borderRight: (columnIndex !== rowData.length - 1) && '1px solid #DFE3EA', borderBottom: '1px solid #DFE3EA',
+                                                }}
                                                 >
-                                                    {data}
-                                                </td>
-                                            )
-                                        })}
-                                    </tr>
-                                )
+                                                {key}
+                                            </td>
+                                        );})
+                                    }
+                                </tr>
+                            );
                             })}
                         </tbody>
                     </table>
                 </div>
-                <div style={{ display: 'flex', justifyContent: IS_MOBILE ? 'flex-start' : 'center', margin: 'auto', width: '90%', overflowX: 'auto' }}>
+                <div style={{ display: 'flex', justifyContent: IS_MOBILE ? 'flex-start' : 'center', margin: '0px auto', width: '90%', overflowX: 'auto' }}>
                     <Pagination
                         activePage={currentPage}
                         itemsCountPerPage={pageCount}
                         totalItemsCount={isSearched ? filteredBooks.length : books.length}
-                        pageRangeDisplayed={10}
+                        pageRangeDisplayed={IS_MOBILE ? 3 : 10}
                         onChange={(pageNumber) => this.setState({ currentPage: pageNumber })}
                     />
                 </div>
